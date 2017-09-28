9c1c0e8649ec42eeecdc6ab9b585f515582b3910
bujichong <bujichong@163.com>
2017-09-28 15:30:06 +0800

    完善eyeicon 查看demo

--- css/eyeicon/plus/plus.js
+++ css/eyeicon/plus/plus.js
@@ -5,8 +5,8 @@ var plusHtml = '<link rel="stylesheet" href="plus/plus.css">'+
 		'<label class="lab-item">图标大小：</label>'+
 		'<label class="lab-val"><input class="rad rad-size" type="radio" value="12px" name="iconsize" />12px</label>'+
 		'<label class="lab-val"><input class="rad rad-size" type="radio" value="14px" name="iconsize" />14px</label>'+
-		'<label class="lab-val"><input class="rad rad-size" type="radio" value="16px" name="iconsize" />16px</label>'+
-		'<label class="lab-val"><input class="rad rad-size" type="radio" value="20px" name="iconsize" checked="checked" />20px</label>'+
+		'<label class="lab-val"><input class="rad rad-size" type="radio" value="16px" name="iconsize" checked="checked" />16px</label>'+
+		'<label class="lab-val"><input class="rad rad-size" type="radio" value="20px" name="iconsize" />20px</label>'+
 		'<label class="lab-val"><input class="rad rad-size" type="radio" value="32px" name="iconsize" />32px</label>'+
 	'</div>'+
 '</div>';
